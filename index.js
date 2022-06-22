cb0458b570190bd77d5572a04044bb93e4d626ea
WadeTheDev <echo1006j@gmail.com>
2022-06-22 16:42:09 +0200

    exercices

---
+++ index.js
@@ -0,0 +1,99 @@
+
+    // 01
+// const fruits = ["mango","lemon","blueburry"]
+// console.log(fruits);
+
+    // 02
+// const ingredients = ["egg","milk","butter"]
+// console.log(ingredients[1]);
+// console.log(ingredients.indexOf("butter"))
+
+//     03
+//  const object = ["pen", "book", "lamp"]
+//  console.log(object);
+
+//  object.unshift("chair")
+//  console.log(object)
+
+//  object.pop("lamp")
+//  console.log(object)
+
+//  object.push("laptop")
+//  console.log(object);
+
+//  object.shift("chair")
+//  console.log(object)
+
+    // // 04
+    // const number = [4, 10, 8, 12, 6]
+    // number.reverse()
+    // console.log
+
+    // 05
+    // let total = 0
+    // const limit = 10
+    // for (let i = 0; i <= limit; i++) {
+    //     total = i + total
+    //     console.log(total)        
+    // }
+
+    // 06
+    // const sentence = "Hello Konexio"
+    // const lengthOfsentense = sentence.length
+    // // let character = sentence.charAt(lengthOfsentense - 1)
+    
+    // for (let i = 13 ; i >= 0; i--) {
+    //    let character = sentence.charAt(i - 1)
+    //     console.log(character);
+    // }
+
+    // 06 (2)
+    // const sentence = "Hello Konexio"
+    // const theArray = sentence.split("")
+    // const lenght = theArray.length
+    // const secondArray = []
+
+    // for (let i = sentence.length ; i >= 0; i--) {
+        
+    //     let character = sentence.charAt(i - 1)
+    //     secondArray.push(character)
+    // }
+    // console.log(secondArray.join(""));
+
+    // 07
+    // for (let i = 0; i <= 100; i++) {
+    //     if (i % 3 === 0 && i % 5 === 0) {
+    //         console.log("fizzbuzz")
+    //     }else if (i % 3 === 0) {
+    //         console.log("fizz")
+    //     }else if (i % 5 === 0) {
+    //         console.log("buzz");
+    //     }
+    //     console.log(i);        
+    // }
+
+    //   08
+    // let total = 0
+    // const limit = 10
+    // let i = 0
+    // while (i < limit) {
+    //     i += 1
+    //     total = i + total
+    //     console.log(total)
+    // }
+
+    // 09
+    // const names = ["Thomas", "Mago", "Walid", "Aurélien", "Dahn"]
+    // const namesLength = names.length
+    // const random = Math.floor(Math.random() * (namesLength - 0)) 
+    // const theSlice = names.slice(random, random + 1)
+    // console.log(theSlice);
+
+    // 10
+    // let numbers = []
+    // let numbersLength = numbers.length
+    
+    // for (let i = 0; i <= 20; i++) {
+    //     numbers[i] = Math.floor(Math.random() * (100 - 0)) 
+    // }
+    // console.log(numbers)
